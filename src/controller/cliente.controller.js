914c56a0d37f09dab5284e0b83cbc407bcee2c7e
MateoBernal06 <133398097+MateoBernal06@users.noreply.github.com>
2025-12-14 22:20:24 -0500

    Login cliente, implementacion de hash

--- src/controller/cliente.controller.js
+++ src/controller/cliente.controller.js
@@ -1,85 +1,78 @@
+const {createClient, findClient,findPhone, login} = require("../model/cliente.model.js");
+const bcrypt = require("bcrypt");
 
-const {createClient, findClient, findPhone} = require('../model/cliente.model.js')
-const bcrypt = require('bcrypt')
-const salt = 12
-
-const registro = async(req, res) => {
+const registro = async (req, res) => {
     try {
         const { name_client, surname_client, phone, email, password } = req.body;
 
-        //const hash = await bcrypt.hash(password, salt)
+        // permite general palabras aleatorias que se colocaran en el hash, evitando que el hash 
+        // se repita en el caso de que varios usuarios posean la misma contraseña
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
 
         const data = {
             name_client: name_client.trim(),
             surname_client: surname_client.trim(),
             phone: phone.trim(),
             email: email.trim(),
-            password: password.trim()
+            password: password.trim(),
         };
 
-        if(!data.name_client || !data.surname_client || !data.phone || !data.email ||!data.password){
-            return res
-                .status(400)
-                .json({
-                    status : false,
-                    msg : `Todos los campos son obligatorios`
-                })
+        if (!data.name_client || !data.surname_client || !data.phone || !data.email || !data.password) {
+            return res.status(400).json({
+                ok: false,
+                msg: `Todos los campos son obligatorios`,
+            });
         }
 
-        const checkEmail = await findClient(data.email)
-        if(checkEmail){
-            return res
-                .status(400)
-                .json({
-                    status: false,
-                    msg: 'El correo ya se encuentra registrado'
-                })
+        const checkEmail = await findClient(data.email);
+        if (checkEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El correo ya se encuentra registrado",
+            });
         }
 
-        const checkPhone = await findPhone(data.phone)
+        const checkPhone = await findPhone(data.phone);
         if (checkPhone) {
             return res.status(400).json({
-                status: false,
+                ok: false,
                 msg: "El numero de celular ya se encuentra registrado",
             });
         }
 
-
-        if(data.phone.length < 10){
+        if (data.phone.length!=10) {
             return res.status(400).json({
-                status: false,
-                msg: `El numero celular debe tener 10 digitos`,
+                ok: false,
+                msg: "El numero celular debe tener 10 digitos",
             });
         }
 
-        if (data.password.length < 8) {
+        if (data.password.length<8 || data.password.length>15) {
             return res.status(400).json({
-                status: false,
-                msg: "La contraseña debe contener minimo 8 digitos",
+                ok: false,
+                msg: "La contraseña debe contener de 8 a 15 caracteres",
             });
         }
 
-        const result = await createClient(data)
-        if(result){
-            console.log(result)
-        }
-        /*data.password = hash*/
+        data.password = hash
+        await createClient(data);
+        
         res.status(200).json({
-            status : true,
-            msg : `Usuario registrado exitosamente`
-        })
-
+            ok: true,
+            msg: "Usuario registrado exitosamente",
+        });
+        
     } catch (error) {
         res.status(500).json({
-            status: false,
-            msg: `Server Error`
+            ok: false,
+            msg: `Server Error`,
         });
     }
-}
+};
 
-
-const loginClient = async(req, res) => {
-    /*try {
+const loginClient = async (req, res) => {
+    try {
         const {email, password}=req.body
 
         const data = {
@@ -90,36 +83,36 @@ const loginClient = async(req, res) => {
         if(!data.email || !data.password){
             res.status(400)
                 .json({
-                    status : true,
-                    msg : 'Todos los campos son obligatorios'
+                    ok: false,
+                    msg: 'Todos los campos son obligatorios'
                 })
         }
 
-        //const result = await loginClient(data)
+        const result = await login(data)
 
         if(!result){
             res.status(400)
                 .json({
-                    status : true,
-                    msg : 'Datos incorrectos'
+                    ok: false,
+                    msg: 'Datos incorrectos'
                 })
         }
 
         res.status(200)
             .json({
-                status : true,
+                ok : true,
                 result
             })
         
     } catch (error) {
         res.status(500).json({
-            status: false,
-            msg: `Server Error`,
+            ok: false,
+            msg: `Server Error`
         });
-    }*/
-}
+    }
+};
 
 module.exports = {
     registro,
-    loginClient
-}
+    loginClient,
+};
--- src/model/cliente.model.js
+++ src/model/cliente.model.js
@@ -1,4 +1,3 @@
-const { text } = require("express");
 const connection = require("../database/connetion.database.js");
 require('colors')
 const bcrypt = require('bcrypt')
@@ -7,21 +6,15 @@ const createClient = async (data) => {
     try {
         //1. Descestructuracion de datos
         const { name_client, surname_client, phone, email, password } = data;
-    
+
         // 2. Creacion de query
         //? Consultas parametrisadas con el fin de evitar inyecciones SQL
-        const query = {
-            text: `
-                    INSERT INTO Cliente (name_client, surname_client, phone, email, password)
-                    VALUES (?, ?, ?, ?, ?)
-                `,
-            values: [name_client, surname_client, phone, email, password],
-        };
-    
-        const [result, fields] = await connection.execute(
-            query.text,
-            query.values
-        );
+        const mysql = ` INSERT INTO Cliente (name_client, surname_client, phone, email, password)
+                        VALUES (?, ?, ?, ?, ?)`;
+        
+        // 3. Valores a ingresar a la base de datos
+        const values = [name_client, surname_client, phone, email, password];
+        const [result, fields] = await connection.execute(mysql, values);
         return result
         
     } catch (error) {
@@ -29,17 +22,41 @@ const createClient = async (data) => {
     }
 }
 
+
+const login = async(data) => {
+    try {
+        const { email, password } = data;
+        const mysql = `SELECT * FROM Cliente WHERE email=?`;
+        const values = [email]
+        const [result, fields] = await connection.execute(mysql, values);
+        const verificate = await bcrypt.compare(password, result[0].password);
+        
+        if(verificate){
+            const { id_client, name_client, surname_client, phone, email, date_register} = result[0];
+            const data = {
+                id_client,
+                name_client,
+                surname_client,
+                phone,
+                email,
+                date_register,
+            };
+            return data
+        }
+        
+        return false
+        
+    } catch (error) {
+        console.log(`Errorr inesperado: ${error.message}`)
+    }
+}
+
 const findClient = async(data) =>{
     try {
         const email = data
-        const query = {
-            text: `
-                SELECT * FROM Cliente WHERE email=?
-            `,
-            values: [email]
-        }
-        
-        const [result, fields] = await connection.execute(query.text, query.values);
+        const mysql = `SELECT * FROM Cliente WHERE email=?`;
+        const values = [email]
+        const [result, fields] = await connection.execute(mysql, values);
         return result[0]
 
     } catch (error) {
@@ -51,15 +68,11 @@ const findClient = async(data) =>{
 const findPhone = async (data) => {
     try {
         const phone = data;
-        const query = {
-        text: `
-                    SELECT * FROM Cliente WHERE phone=?
-                `,
-        values: [phone],
-        };
-
-        const [result, fields] = await connection.execute(query.text, query.values);
+        const mysql = `SELECT * FROM Cliente WHERE phone=?`;
+        const values = [phone]
+        const [result, fields] = await connection.execute(mysql, values);
         return result[0];
+
     } catch (error) {
         console.log(`Error inesperado: ${error.message}`);
     }
@@ -68,7 +81,8 @@ const findPhone = async (data) => {
 module.exports = {
     createClient,
     findClient,
-    findPhone
+    findPhone,
+    login
 };
 
 
